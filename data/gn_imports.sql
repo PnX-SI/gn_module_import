78b043fb6a3a0e48feb3ab4beb3d5bcfa9272da0
ju <julien_corny@natural-solutions.eu>
2019-10-04 11:53:27 +0200

    bib_themes and bib_fields filled (insert into)

--- data/gn_imports.sql
+++ data/gn_imports.sql
@@ -84,7 +84,8 @@ CREATE TABLE bib_type_mapping_values(
 CREATE TABLE bib_themes(
     id_theme integer,
     name_theme character varying(100) NOT NULL,
-    label_theme character varying(100) NOT NULL,
+    fr_label character varying(100) NOT NULL,
+    eng_label character varying(100),
     desc_theme character varying(1000),
     order_theme integer NOT NULL
 );
@@ -96,7 +97,8 @@ CREATE TABLE bib_fields(
     fr_label character varying(100) NOT NULL,
     eng_label character varying(100),
     desc_field character varying(1000),
-    type_field character varying(50) NOT NULL,
+    type_field character varying(50),
+    synthese_field boolean NOT NULL,
     mandatory boolean NOT NULL,
     autogenerate boolean NOT NULL,
     nomenclature boolean NOT NULL,
@@ -212,13 +214,58 @@ INSERT INTO user_errors (id_error, error_type, name, description) VALUES
 	(13, 'inconsistency_error', 'inconsistent geographic coordinate', 'coordonnée géographique incohérente');
 
 
+INSERT INTO bib_themes (id_theme, name_theme, fr_label, eng_label, desc_theme, order_theme) VALUES
+	(1, 'general_info', 'Informations générales', '', '', 1),
+    (2, 'statement_info', 'Informations de relevés', '', '', 2),
+    (3, 'occurrence_sensitivity', 'Informations d''occurrences & sensibilité', '', '', 3),
+    (4, 'enumeration', 'Dénombrements', '', '', 4),
+    (5, 'validation', 'Détermination et validité', '', '', 5);
 
 
-
-
-
-
-
-
+INSERT INTO bib_fields (id_field, name_field, fr_label, eng_label, desc_field, type_field, synthese_field, mandatory, autogenerate, nomenclature, id_theme, order_field) VALUES
+	(1, 'entity_source_pk_value', 'Identifiant source', '', '', 'character varying', TRUE, FALSE, FALSE, FALSE, 1, 1),
+	(2, 'unique_id_sinp', 'Identifiant SINP (uuid)', '', '', 'uuid', TRUE, FALSE, FALSE, FALSE, 1, 2),
+	(3, 'unique_id_sinp_generate', 'Générer l''identifiant SINP', '', 'Génère automatiquement un identifiant de type uuid pour chaque observation', '', FALSE, FALSE, TRUE, FALSE, 1, 3), 
+	(4, 'meta_create_date', 'Date de création de la donnée', '', '', 'timestamp without time zone', TRUE, FALSE, FALSE, FALSE, 1, 4),
+	(5, 'meta_v_taxref', 'Version du référentiel taxonomique', '', '', 'character varying(50)', TRUE, FALSE, FALSE, FALSE, 1, 5),
+	(6, 'meta_update_date', 'Date de mise à jour de la donnée', '', '', 'timestamp without time zone', TRUE, FALSE, FALSE, FALSE, 1, 6),
+	(7, 'date_min', 'Date début', '', '', 'timestamp without time zone', TRUE, TRUE, FALSE, FALSE, 2, 1),
+	(8, 'date_max', 'Date fin', '', '', 'timestamp without time zone', TRUE, FALSE, FALSE, FALSE, 2, 2),
+	(9, 'altitude_min', 'Altitude min', '', '', 'integer', TRUE, FALSE, FALSE, FALSE, 2, 3),
+	(10, 'altitude_max', 'Altitude max', '', '', 'integer', TRUE, FALSE, FALSE, FALSE, 2, 4),
+	(11, 'altitudes_generate', 'Générer les altitudes', '', 'Génère automatiquement les altitudes pour chaque observation', '', FALSE, FALSE, TRUE, FALSE, 2, 5),
+	(12, 'longitude', 'Longitude (coord x)', '', '', '', FALSE, TRUE, FALSE, FALSE, 2, 6),
+	(13, 'latitude', 'Latitude (coord y)', '', '', '', FALSE, TRUE, FALSE, FALSE, 2, 7),
+	(14, 'observers', 'Observateur(s)', '', '', 'character varying(1000)', TRUE, FALSE, FALSE, FALSE, 2, 8),
+	(15, 'comment_description', 'Commentaire de relevé', '', '', 'text', TRUE, FALSE, FALSE, FALSE, 2, 9),
+	(16, 'id_nomenclature_info_geo_type', 'Type d''information géographique', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 2, 10),
+	(17, 'id_nomenclature_grp_typ', 'Type de relevé/regroupement', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 2, 11),
+	(18, 'nom_cite', 'Nom du taxon cité', '', '', 'character varying(1000)', TRUE, TRUE, FALSE, FALSE, 3, 1),
+	(19, 'cd_nom', 'Cd nom taxref', '', '', 'integer', TRUE, TRUE, FALSE, FALSE, 3, 2),
+	(20, 'id_nomenclature_obs_meth', 'Méthode d''observation', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 3, 3),
+	(21, 'id_nomenclature_bio_status', 'Statut biologique', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 3, 4),
+	(22, 'id_nomenclature_bio_condition', 'Etat biologique', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 3, 5),
+	(23, 'id_nomenclature_naturalness', 'Naturalité', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 3, 6),
+	(24, 'comment_context', 'Commentaire d''occurrence', '', '', 'text', TRUE, FALSE, FALSE, FALSE, 3, 7),
+	(25, 'id_nomenclature_sensitivity', 'Sensibilité', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 3, 8),
+	(26, 'id_nomenclature_diffusion_level', 'Niveau de diffusion', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 3, 9),
+	(27, 'id_nomenclature_blurring', 'Niveau de Floutage', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 3, 10),
+	(28, 'id_nomenclature_life_stage', 'Stade de vie', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 4, 1),
+	(29, 'id_nomenclature_sex', 'Sexe', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 4, 2),
+	(30, 'id_nomenclature_type_count', 'Type du dénombrement', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 4, 3),
+	(31, 'id_nomenclature_obj_count', 'Objet du dénombrement', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 4, 4),
+	(32, 'count_min', 'Nombre minimal', '', '', 'integer', TRUE, FALSE, FALSE, FALSE, 4, 5),
+	(33, 'count_max', 'Nombre maximal', '', '', 'integer', TRUE, FALSE, FALSE, FALSE, 4, 6),
+	(34, 'id_nomenclature_determination_method', 'Méthode de détermination', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 5, 1),
+	(35, 'determiner', 'Déterminateur', '', '', 'character varying(1000)', TRUE, FALSE, FALSE, FALSE, 5, 2),
+	(36, 'id_digitiser', 'Auteur de la saisie', '', '', 'integer', TRUE, FALSE, FALSE, FALSE, 5, 3),
+	(37, 'id_nomenclature_exist_proof', 'Existance d''une preuve', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 5, 4),
+	(38, 'digital_proof', 'Preuve numérique', '', '', 'text', TRUE, FALSE, FALSE, FALSE, 5, 5),
+	(39, 'non_digital_proof', 'Preuve non-numérique', '', '', 'text', TRUE, FALSE, FALSE, FALSE, 5, 6),
+	(40, 'sample_number_proof', 'Identifiant de l''échantillon preuve', '', '', 'text', TRUE, FALSE, FALSE, FALSE, 5, 7),
+	(41, 'id_nomenclature_valid_status', 'Statut de validation', '', '', 'integer', TRUE, FALSE, FALSE, TRUE, 5, 8),
+	(42, 'validator', 'Validateur', '', '', 'character varying(1000)', TRUE, FALSE, FALSE, FALSE, 5, 9),
+	(43, 'meta_validation_date', 'Date de validation', '', '', 'timestamp without time zone', TRUE, FALSE, FALSE, FALSE, 5, 10),
+	(44, 'validation_comment', 'Commentaire de validation', '', '', 'text', TRUE, FALSE, FALSE, FALSE, 5, 11);
 
 
