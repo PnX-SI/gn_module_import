3ac003f5e15fc30304839a86c8d8a40ab34f66db
ju <julien_corny@natural-solutions.eu>
2019-11-13 17:04:32 +0100

    spinner in import step

--- frontend/app/components/import_process/import-step/import-step.component.ts
+++ frontend/app/components/import_process/import-step/import-step.component.ts
@@ -2,16 +2,17 @@ import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { StepsService } from '../steps.service';
 import { DataService } from '../../../services/data.service';
 import { ToastrService } from 'ngx-toastr';
-//import { ModuleConfig } from '../../../module.config';
+import { Router } from '@angular/router';
+import { ModuleConfig } from '../../../module.config';
 import * as _ from 'lodash';
 
 @Component({
-	selector: 'import-step',
-	styleUrls: [ 'import-step.component.scss' ],
-	templateUrl: 'import-step.component.html'
+    selector: 'import-step',
+    styleUrls: ['import-step.component.scss'],
+    templateUrl: 'import-step.component.html'
 })
 export class ImportStepComponent implements OnInit, OnChanges {
-	public isCollapsed = false;
+    public isCollapsed = false;
     @Input() selected_columns: any;
     @Input() added_columns: any;
     @Input() importId: any;
@@ -19,82 +20,93 @@ export class ImportStepComponent implements OnInit, OnChanges {
     importDataRes: any;
     validData: any;
     total_columns: any;
-	columns: any[] = [];
-	rows: any[] = [];
-	tableReady: boolean = false;
+    columns: any[] = [];
+    rows: any[] = [];
+    tableReady: boolean = false;
+    public spinner: boolean = false;
 
-	constructor(private stepService: StepsService, private _ds: DataService, private toastr: ToastrService) {}
 
-	ngOnInit() {}
+    constructor(
+        private stepService: StepsService,
+        private _router: Router,
+        private _ds: DataService,
+        private toastr: ToastrService
+    ) { }
 
-	ngOnChanges() {
-		if (this.stepId == 'three') {
-			this.getValidData();
-		}
-	}
+    ngOnInit() { }
 
-	onStepBack() {
-		this.stepService.previousStep();
-	}
+    ngOnChanges() {
+        if (this.stepId == 'three') {
+            this.getValidData();
+        }
+    }
 
-	onImport() {
-		this._ds.importData(this.importId, this.total_columns).subscribe(
-			(res) => {
-				this.importDataRes = res;
-				console.log(this.importDataRes);
-			},
-			(error) => {
-				if (error.statusText === 'Unknown Error') {
-					// show error message if no connexion
-					this.toastr.error('ERROR: IMPOSSIBLE TO CONNECT TO SERVER (check your connexion)');
-				} else {
-					// show error message if other server error
-					console.log(error);
-					this.toastr.error(error.error.message + ' = ' + error.error.details);
-				}
-			}
-		);
-	}
+    onStepBack() {
+        this.stepService.previousStep();
+    }
 
-	getValidData() {
-		this._ds.getValidData(this.importId, this.selected_columns, this.added_columns).subscribe(
-			(res) => {
-				this.validData = res.valid_data;
-				this.total_columns = res['total_columns'];
+    onImport() {
+        this.spinner = true;
+        this._ds.importData(this.importId, this.total_columns).subscribe(
+            (res) => {
+                this.spinner = false;
+                this.importDataRes = res;
+                console.log(this.importDataRes);
+                this._router.navigate([ `${ModuleConfig.MODULE_URL}` ]);
+            },
+            (error) => {
+                this.spinner = false;
+                if (error.statusText === 'Unknown Error') {
+                    // show error message if no connexion
+                    this.toastr.error('ERROR: IMPOSSIBLE TO CONNECT TO SERVER (check your connexion)');
+                } else {
+                    // show error message if other server error
+                    console.log(error);
+                    this.toastr.error(error.error.message + ' = ' + error.error.details);
+                }
+            }
+        );
+    }
 
-				_.forEach(this.validData[0], (el) => {
-					let key = el.key;
-					let val = el.value;
-					this.columns.push({ name: key, prop: key });
-				});
+    getValidData() {
+        this._ds.getValidData(this.importId, this.selected_columns, this.added_columns).subscribe(
+            (res) => {
+                this.validData = res.valid_data;
+                this.total_columns = res['total_columns'];
 
-				_.forEach(this.validData, (data) => {
-					let obj = {};
-					_.forEach(data, (el) => {
-						let key = el.key;
-						let val = el.value;
-						obj[key] = val;
-					});
-					this.rows.push(obj);
-				});
+                _.forEach(this.validData[0], (el) => {
+                    let key = el.key;
+                    let val = el.value;
+                    this.columns.push({ name: key, prop: key });
+                });
 
-				this.tableReady = true;
-			},
-			(error) => {
-				if (error.statusText === 'Unknown Error') {
-					// show error message if no connexion
-					this.toastr.error('ERROR: IMPOSSIBLE TO CONNECT TO SERVER (check your connexion)');
-				} else {
-					// show error message if other server error
-					console.log(error);
-					this.toastr.error(error.error.message);
-				}
-			}
-		);
+                _.forEach(this.validData, (data) => {
+                    let obj = {};
+                    _.forEach(data, (el) => {
+                        let key = el.key;
+                        let val = el.value;
+                        obj[key] = val;
+                    });
+                    this.rows.push(obj);
+                });
+
+                this.tableReady = true;
+            },
+            (error) => {
+                if (error.statusText === 'Unknown Error') {
+                    // show error message if no connexion
+                    this.toastr.error('ERROR: IMPOSSIBLE TO CONNECT TO SERVER (check your connexion)');
+                } else {
+                    // show error message if other server error
+                    console.log(error);
+                    this.toastr.error(error.error.message);
+                }
+            }
+        );
     }
 
 
-    }
+}
         
 
     
