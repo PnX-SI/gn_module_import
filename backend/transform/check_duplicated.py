febfe595f6264ecfd28aeb10c846eaec39ccd1ec
ju <julien_corny@natural-solutions.eu>
2020-02-05 17:32:51 +0100

    bug fix in check_row_duplicates() for checkbox fields

--- backend/transform/check_duplicated.py
+++ backend/transform/check_duplicated.py
@@ -6,13 +6,19 @@ from .utils import fill_map, set_is_valid, set_invalid_reason
 from ..wrappers import checker
 from ..logs import logger
 
+import pdb
 
 @checker('Data cleaning : row duplicates checked')
 def check_row_duplicates(df, selected_columns, import_id, schema_name):
     try:
         logger.info('CHECKING FOR DUPLICATED ROWS : ')
+        selected_cols = selected_columns
+        generate_fields = ['unique_id_sinp_generate', 'altitudes_generate']
+        for field in generate_fields:
+            if field in selected_cols.keys():
+                del selected_cols[field]
 
-        df['temp'] = df.duplicated(subset=selected_columns.values(), keep=False)
+        df['temp'] = df.duplicated(subset=selected_cols.values(), keep=False)
         df['temp'] = ~df['temp']
         set_is_valid(df, 'temp')
         n_duplicated_rows = df['temp'].astype(str).str.contains('False').sum()
