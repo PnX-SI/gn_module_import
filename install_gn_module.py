0968ca53229c127034c83a77fe81dad800360503
DonovanMaillard <donovan.maillard@gmail.com>
2020-02-19 10:05:53 +0100

    Update install_gn_module.py
    Mise à jour de l'installation en s'appuyant sur les 3 fichiers sql scindés

--- install_gn_module.py
+++ install_gn_module.py
@@ -15,9 +15,12 @@ def gnmodule_install_app(gn_db, gn_app):
     with gn_app.app_context():
         # install db schema and tables
         gn_db.session.execute(
-            open(str(ROOT_DIR / "data/gn_imports.sql"), "r").read()
+            open(str(ROOT_DIR / "data/import_db.sql"), "r").read()
         )
         gn_db.session.execute(
-            open(str(ROOT_DIR / "data/gn_import_archives.sql"), "r").read()
+            open(str(ROOT_DIR / "data/data.sql"), "r").read()
+        )
+        gn_db.session.execute(
+            open(str(ROOT_DIR / "data/default_mappings_data.sql"), "r").read()
         )
         gn_db.session.commit()
