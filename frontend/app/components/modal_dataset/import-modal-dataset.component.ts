39c31bee5ae6b57cd4e847f962638666811e26c7
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-02-19 10:56:19 +0100

    Utilisation du composant dataset pour afficher les JDD actifs close #79

--- frontend/app/components/modal_dataset/import-modal-dataset.component.ts
+++ frontend/app/components/modal_dataset/import-modal-dataset.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { CommonService } from "@geonature_common/service/common.service";
 import { Router } from "@angular/router";
 import { DataService } from "../../services/data.service";
@@ -13,7 +13,7 @@ import { StepsService } from "../import_process/steps.service";
   styleUrls: ["./import-modal-dataset.component.scss"]
 })
 export class ImportModalDatasetComponent implements OnInit, OnDestroy {
-  public selectDatasetForm: FormGroup;
+  public selectDatasetForm: FormControl;
   public userDatasetsResponse: any;
   public datasetResponse: JSON;
   public isUserDatasetError: Boolean = false; // true if user does not have any declared dataset
@@ -22,19 +22,14 @@ export class ImportModalDatasetComponent implements OnInit, OnDestroy {
 
   constructor(
     private modalService: NgbModal,
-    private _fb: FormBuilder,
     public _ds: DataService,
     private _commonService: CommonService,
     private stepService: StepsService,
     private _router: Router //private _idImport: importIdStorage
-  ) {
-    this.selectDatasetForm = this._fb.group({
-      dataset: ["", Validators.required]
-    });
-  }
+  ) {}
 
   ngOnInit() {
-    this.getUserDataset();
+    this.selectDatasetForm = new FormControl(null, Validators.required);
   }
 
   onOpenModal(content) {
@@ -54,31 +49,6 @@ export class ImportModalDatasetComponent implements OnInit, OnDestroy {
       queryParams: { datasetId: this.selectDatasetForm.value.dataset }
     });
     this.closeModal();
-  }
-
-  getUserDataset() {
-    // get list of all declared dataset of the user
-    this._ds.getUserDatasets().subscribe(
-      result => {
-        this.userDatasetsResponse = result;
-      },
-      err => {
-        if (err.statusText === "Unknown Error") {
-          // show error message if no connexion
-          this._commonService.regularToaster(
-            "error",
-            "ERROR: IMPOSSIBLE TO CONNECT TO SERVER (check your connexion)"
-          );
-        } else {
-          // show error message if user does not have any declared dataset
-          if (err.status == 400) {
-            this.isUserDatasetError = true;
-            this.datasetError = err.error;
-          }
-          this._commonService.regularToaster("error", err.error.message);
-        }
-      }
-    );
   }
 
   ngOnDestroy(): void {
