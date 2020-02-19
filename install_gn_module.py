ec8b4b94efeb351a1f1c13d6d8054a09021bf02b
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-02-19 10:42:19 +0100

    Raise et affiche l'erreur quans le sql d'install bug

--- install_gn_module.py
+++ install_gn_module.py
@@ -1,8 +1,12 @@
 import subprocess
-from pathlib import Path
 import sys
 
+import logging
+from pathlib import Path
+
+
 ROOT_DIR = Path(__file__).absolute().parent
+log = logging.getLogger(__name__)
 
 
 def gnmodule_install_app(gn_db, gn_app):
@@ -14,13 +18,17 @@ def gnmodule_install_app(gn_db, gn_app):
 
     with gn_app.app_context():
         # install db schema and tables
-        gn_db.session.execute(
-            open(str(ROOT_DIR / "data/import_db.sql"), "r").read()
-        )
-        gn_db.session.execute(
-            open(str(ROOT_DIR / "data/data.sql"), "r").read()
-        )
-        gn_db.session.execute(
-            open(str(ROOT_DIR / "data/default_mappings_data.sql"), "r").read()
-        )
-        gn_db.session.commit()
+        try:
+            gn_db.session.execute(
+                open(str(ROOT_DIR / "data/import_db.sql"), "r").read()
+            )
+            gn_db.session.execute(
+                open(str(ROOT_DIR / "data/data.sql"), "r").read()
+            )
+            gn_db.session.execute(
+                open(str(ROOT_DIR / "data/default_mappings_data.sql"), "r").read()
+            )
+            gn_db.session.commit()
+        except Exception as e:
+            log.error(e)
+            raise
