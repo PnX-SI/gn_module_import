bad1cf5d31b5c4d10675c2a56777ff5a41cf306d
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-04-07 10:39:07 +0200

    SQL: modification du MCD pour amélioration des rapports d'erreurs #114

--- data/migration/1.0.0to1.1.0.sql
+++ data/migration/1.0.0to1.1.0.sql
@@ -5,3 +5,6 @@ ADD COLUMN error_level character varying(25)
 
 ALTER TABLE gn_imports.t_user_error_list
 ADD COLUMN id_rows integer[];
+
+ALTER TABLE gn_imports.t_imports
+ADD COLUMN error_report_path character varying(255);
