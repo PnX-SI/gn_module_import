c5825b70819bc22700ff1edcaec5b75cba2e6c79
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-04-08 11:43:53 +0200

    Ajout d'une vue pour l'affichage des erreurs #114

--- data/import_db.sql
+++ data/import_db.sql
@@ -132,6 +132,20 @@ CREATE TABLE t_user_error_list(
 );
 
 
+CREATE VIEW gn_imports.v_imports_errors AS 
+SELECT 
+id_user_error,
+id_import,
+error_type,
+name AS error_name,
+error_level,
+description AS error_description,
+column_error,
+id_rows
+FROM  gn_imports.t_user_error_list el 
+JOIN gn_imports.t_user_errors ue on ue.id_error = el.id_error;
+
+
 
 ----------------------------
 --PRIMARY KEY AND UNICITY---
--- data/migration/1.0.0to1.1.0.sql
+++ data/migration/1.0.0to1.1.0.sql
@@ -9,3 +9,17 @@ DROP COLUMN count_error;
 
 ALTER TABLE gn_imports.t_imports
 ADD COLUMN error_report_path character varying(255);
+
+
+CREATE VIEW gn_imports.v_imports_errors AS 
+SELECT 
+id_user_error,
+id_import,
+error_type,
+name AS error_name,
+error_level,
+description AS error_description,
+column_error,
+id_rows
+FROM  gn_imports.t_user_error_list el 
+JOIN gn_imports.t_user_errors ue on ue.id_error = el.id_error;
