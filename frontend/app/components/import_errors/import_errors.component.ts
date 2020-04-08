8b6bc0ecec786829644adfca7ba592f999f97555
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-04-08 17:53:40 +0200

    Ajout d'une page 'rapport d'erreur' accessible depuis la liste des imports #118

---
+++ frontend/app/components/import_errors/import_errors.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { DataService } from '../../services/data.service'
+
+@Component({
+    selector: 'pnx-import-errors',
+    templateUrl: 'import_errors.component.html',
+    styleUrls: ["import_errors.component.scss"],
+
+})
+
+export class ImportErrorsComponent implements OnInit, OnDestroy {
+    private sub: any;
+    public import: any;
+    constructor(private _dataService: DataService, private _activedRoute: ActivatedRoute) { }
+
+    ngOnInit() {
+
+        this.sub = this._activedRoute.params.subscribe(params => {
+            console.log(params);
+            this._dataService.getOneImport(
+                params["id_import"]
+            ).subscribe(data => {
+                this.import = data;
+            })
+
+        })
+    }
+
+    // ngAfterViewInit() {
+    //     console.log(this._activedRoute);
+
+
+    // }
+
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+}
