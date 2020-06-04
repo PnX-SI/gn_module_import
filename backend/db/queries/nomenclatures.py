456e93f6e03bf6991419febea33b8ec38a80e431
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-06-04 14:46:39 +0200

    Mappings: ordonnancement des listes par ordre alphabétique

--- backend/db/queries/nomenclatures.py
+++ backend/db/queries/nomenclatures.py
@@ -52,6 +52,7 @@ def get_nomenc_values(nommenclature_abb):
         FROM ref_nomenclatures.bib_nomenclatures_types AS bib
         JOIN ref_nomenclatures.t_nomenclatures AS nom ON nom.id_type = bib.id_type
         WHERE bib.mnemonique = :nomenc
+        ORDER BY nomenc_values ASC
         """
     return DB.session.execute(text(query), {"nomenc": nommenclature_abb}).fetchall()
 
@@ -102,7 +103,8 @@ def get_synthese_col(abb):
         LEFT JOIN ref_nomenclatures.bib_nomenclatures_types BNT ON BNT.mnemonique = CSN.mnemonique
         WHERE BNT.mnemonique = :abb;
     """
-    nomenc_synthese_name = DB.session.execute(text(query), {"abb": abb}).fetchone()
+    nomenc_synthese_name = DB.session.execute(
+        text(query), {"abb": abb}).fetchone()
     return nomenc_synthese_name.synthese_name
 
 
@@ -199,7 +201,8 @@ def get_nomenc_abb_from_name(synthese_name):
 
 def set_default_value(abb):
     default_value = DB.session.execute(
-        text("SELECT gn_synthese.get_default_nomenclature_value(:abb)"), {"abb": abb},
+        text("SELECT gn_synthese.get_default_nomenclature_value(:abb)"), {
+            "abb": abb},
     ).fetchone()[0]
     if default_value is None:
         default_value = "NULL"
@@ -224,7 +227,8 @@ def set_default_nomenclature_id(table_name, nomenc_abb, user_col, id_types):
         user_col=user_col,
     )
     DB.session.execute(
-        text(query), {"default_value": default_value, "id_types": tuple(id_types)}
+        text(query), {"default_value": default_value,
+                      "id_types": tuple(id_types)}
     )
 
 
@@ -294,7 +298,7 @@ def get_saved_content_mapping(id_mapping):
     return selected_content
 
 
-####### conditional check
+#  conditional check
 
 
 def exist_proof_check(
@@ -353,7 +357,8 @@ def dee_bluring_check(table_name, id_import, bluring_col):
         WHERE id_import = :id_import
         )
     """
-    ds_public = DB.session.execute(query_ds_pub, {"id_import": id_import}).fetchone()
+    ds_public = DB.session.execute(
+        query_ds_pub, {"id_import": id_import}).fetchone()
     if ds_public.code != "Pr":
         return None
     else:
