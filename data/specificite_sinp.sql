cc6fdd9013e44d06e134a5771324dd6baf988bf6
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-07-07 10:29:00 +0200

    Permission plateformes régionales

--- data/specificite_sinp.sql
+++ data/specificite_sinp.sql
@@ -9,6 +9,38 @@ WHERE name_field IN (
 );
 
 
+-- Permissions groupe admin sur import
+INSERT INTO gn_permissions.cor_role_action_filter_module_object
+    (
+    id_role,
+    id_action,
+    id_filter,
+    id_module,
+    id_object
+    )
+VALUES
+    ((SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Grp_admin' AND groupe IS TRUE), 1, 4, (SELECT id_module FROM gn_commons.t_modules WHERE module_code='IMPORT'), 1),
+    ((SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Grp_admin' AND groupe IS TRUE), 2, 4, (SELECT id_module FROM gn_commons.t_modules WHERE module_code='IMPORT'), 1),
+    ((SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Grp_admin' AND groupe IS TRUE), 3, 4, (SELECT id_module FROM gn_commons.t_modules WHERE module_code='IMPORT'), 1),
+    ((SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Grp_admin' AND groupe IS TRUE), 6, 4, (SELECT id_module FROM gn_commons.t_modules WHERE module_code='IMPORT'), 1)
+;
+
+-- groupe producteur import désactivé
+INSERT INTO gn_permissions.cor_role_action_filter_module_object
+    (
+    id_role,
+    id_action,
+    id_filter,
+    id_module,
+    id_object
+    )
+VALUES
+    ((SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Producteur' AND groupe IS TRUE), 1, 1, (SELECT id_module FROM gn_commons.t_modules WHERE module_code='IMPORT'), 1),
+    ((SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Producteur' AND groupe IS TRUE), 2, 1, (SELECT id_module FROM gn_commons.t_modules WHERE module_code='IMPORT'), 1),
+    ((SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Producteur' AND groupe IS TRUE), 3, 1, (SELECT id_module FROM gn_commons.t_modules WHERE module_code='IMPORT'), 1),
+    ((SELECT id_role FROM utilisateurs.t_roles WHERE nom_role = 'Producteur' AND groupe IS TRUE), 6, 1, (SELECT id_module FROM gn_commons.t_modules WHERE module_code='IMPORT'), 1)
+;
+
 
 INSERT INTO gn_permissions.cor_role_action_filter_module_object
     (
