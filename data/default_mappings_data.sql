0c8ce07a3bc6f59f67932fabda6d0e28885cee4b
DonovanMaillard <donovan.maillard@gmail.com>
2020-10-09 11:54:12 +0200

    Enable all nomenclatures in mappings
    not only active

--- data/default_mappings_data.sql
+++ data/default_mappings_data.sql
@@ -106,7 +106,7 @@ n.label_default,
 n.id_nomenclature
 FROM gn_imports.t_mappings m, ref_nomenclatures.t_nomenclatures n
 JOIN ref_nomenclatures.bib_nomenclatures_types bnt ON bnt.id_type=n.id_type 
-WHERE m.mapping_label='Nomenclatures SINP (labels)' AND bnt.mnemonique IN (SELECT DISTINCT(mnemonique) FROM gn_imports.cor_synthese_nomenclature) AND n.active;
+WHERE m.mapping_label='Nomenclatures SINP (labels)' AND bnt.mnemonique IN (SELECT DISTINCT(mnemonique) FROM gn_imports.cor_synthese_nomenclature);
 
 
 -- Intégration du mapping de valeurs SINP (codes) par défaut pour les nomenclatures de la synthèse
