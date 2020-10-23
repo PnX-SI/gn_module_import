9238be51d8f0455248756e885e9ca2c498e2f7b6
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-10-23 10:16:55 +0200

    Email: url correction

--- backend/send_mail.py
+++ backend/send_mail.py
@@ -17,7 +17,8 @@ def import_send_mail(mail_to, file_name, step, id_import):
         :param step: step of the processe: 'import' | 'check'
     """
     if step == 'check':
-        msg = """
+        link = f"{current_app.config['URL_APPLICATION']}/#/import/process/id_import/{str(id_import)/}step/4"
+        msg = f"""
         Bonjour,
         <p>
             Les vérifications sur le fichier {file_name} sont terminés.
@@ -25,21 +26,14 @@ def import_send_mail(mail_to, file_name, step, id_import):
         <p>  Cliquez sur ce <a target="_blank" href="{link}"> lien </a>  
         pour terminer l'import dans la synthese</p>
 
-        """.format(
-            file_name=file_name,
-            link=current_app.config['URL_APPLICATION'] +
-            "/#/import/process/step/4/id_import/"+str(id_import)
-
-        )
+        """
     else:
-        msg = """
+        msg = f"""
         Bonjour,
         <p>
             L'import du fichier {file_name} dans la synthese est terminé.
         </p>
-        """.format(
-            file_name=file_name
-        )
+        """
 
     send_mail(
         recipients=mail_to,
@@ -61,14 +55,14 @@ def import_send_mail_error(mail_to, file_name, error):
         :query str error: Detail of the exception raised
 
     """
-    msg = """
+    msg = f"""
         Bonjour,
         <p>
             Votre import <i>{file_name}</i> n'a pas fonctionné correctement.
         </p>
         <p> {error} </p>
 
-    """.format(file_name=file_name, error=error)
+    """
     send_mail(
         recipients=mail_to,
         subject="[GeoNature][ERREUR] Import {}".format(file_name),
