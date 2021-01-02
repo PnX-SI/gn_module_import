54d3c36801e48ca52c0868e2709574ed7330dc7e
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-01-02 17:24:04 +0100

    fix bug on date

--- backend/transform/set_dates.py
+++ backend/transform/set_dates.py
@@ -52,8 +52,6 @@ def set_dates(df, selected_columns, synthese_info, import_id, schema_name):
                 df[hour_max_col].fillna(df[selected_columns["hour_min"]], inplace=True)
             # calculate hour length
             df['hour_max_length'] = df[hour_max_col].apply(len)
-            # replace unfill hour with hour min and with 00:00:00
-            df[hour_max_col] = df[hour_max_col].where(df['hour_max_length'] != 0, other=df[selected_columns['hour_min' ]])
             df[hour_max_col] = df[hour_max_col].where(df['hour_max_length'] != 0, other='00:00:00')
             df[selected_columns["date_max"]] = (
                 df[selected_columns["date_max"]]
@@ -61,7 +59,18 @@ def set_dates(df, selected_columns, synthese_info, import_id, schema_name):
                 + df[selected_columns["hour_max"]]
             )
             df["concatened_date_max"] = True
-            print(df[selected_columns["date_max"]])
+        if (
+            not "hour_max" in selected_columns
+            and "hour_min" in selected_columns
+            and "date_max" in selected_columns
+            and not "concatened_date_max" in df
+        ):
+            df[selected_columns["date_max"]] = (
+                df[selected_columns["date_max"]]
+                + " "
+                + df[selected_columns["hour_min"]]
+            )
+            df["concatened_date_max"] = True
 
         # set date_max (=if data_max not existing, then set equal to date_min)
         if "date_max" not in date_fields:
