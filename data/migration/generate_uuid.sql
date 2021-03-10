1a628b0c75bc7f63a38da7897d7477b7ff2ff022
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2021-03-10 14:16:24 +0100

    UUID SQL - Add comment

--- data/migration/generate_uuid.sql
+++ data/migration/generate_uuid.sql
@@ -1,3 +1,4 @@
+-- Calculer les UUID pour toutes les données du module Synthèse qui n'en n'ont pas, provenant du module Import
 UPDATE gn_synthese.synthese 
 SET unique_id_sinp = uuid_generate_v4()
 WHERE unique_id_sinp IS NULL AND id_module = (
