d4b2295f43245654f5a848b7806aea38b8a5fa12
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-01-19 15:58:10 +0100

    convert readme from rest to markdown

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pathlib import Path
 root_dir = Path(__file__).absolute().parent
 with (root_dir / 'VERSION').open() as f:
     version = f.read()
-with (root_dir / 'README.rst').open() as f:
+with (root_dir / 'README.md').open() as f:
     long_description = f.read()
 with (root_dir / 'requirements.in').open() as f:
     requirements = f.read().splitlines()
@@ -16,7 +16,7 @@ setuptools.setup(
     version=version,
     description="GeoNature",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
+    long_description_content_type='text/markdown',
     maintainer='Parcs nationaux des Écrins et des Cévennes',
     maintainer_email='geonature@ecrins-parcnational.fr',
     url='https://github.com/PnX-SI/gn_module_import',
