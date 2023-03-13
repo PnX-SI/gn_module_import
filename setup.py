2a2f865440da1aea2ddc5496c3f8a83f0acd0a69
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2023-03-13 09:58:36 +0100

    apply black to setup.py

--- setup.py
+++ setup.py
@@ -3,50 +3,52 @@ from pathlib import Path
 
 
 root_dir = Path(__file__).absolute().parent
-with (root_dir / 'VERSION').open() as f:
+with (root_dir / "VERSION").open() as f:
     version = f.read()
-with (root_dir / 'README.md').open() as f:
+with (root_dir / "README.md").open() as f:
     long_description = f.read()
-with (root_dir / 'requirements.in').open() as f:
+with (root_dir / "requirements.in").open() as f:
     requirements = f.read().splitlines()
 
 
 setuptools.setup(
-    name='gn_module_import',
+    name="gn_module_import",
     version=version,
     description="GeoNature",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    maintainer='Parcs nationaux des Écrins et des Cévennes',
-    maintainer_email='geonature@ecrins-parcnational.fr',
-    url='https://github.com/PnX-SI/gn_module_import',
-    packages=setuptools.find_packages('backend'),
-    package_dir={'': 'backend'},
-    package_data={'gn_module_import.migrations': ['data/*.sql']},
+    long_description_content_type="text/markdown",
+    maintainer="Parcs nationaux des Écrins et des Cévennes",
+    maintainer_email="geonature@ecrins-parcnational.fr",
+    url="https://github.com/PnX-SI/gn_module_import",
+    packages=setuptools.find_packages("backend"),
+    package_dir={"": "backend"},
+    package_data={"gn_module_import.migrations": ["data/*.sql"]},
     install_requires=requirements,
     tests_require=[
-        'jsonschema',
+        "jsonschema",
     ],
     zip_safe=False,
     entry_points={
-        'gn_module': [
-            'code = gn_module_import:MODULE_CODE',
-            'picto = gn_module_import:MODULE_PICTO',
-            'blueprint = gn_module_import.blueprint:blueprint',
-            'config_schema = gn_module_import.conf_schema_toml:GnModuleSchemaConf',
-            'migrations = gn_module_import:migrations',
-            'tasks = gn_module_import.tasks',
+        "gn_module": [
+            "code = gn_module_import:MODULE_CODE",
+            "picto = gn_module_import:MODULE_PICTO",
+            "blueprint = gn_module_import.blueprint:blueprint",
+            "config_schema = gn_module_import.conf_schema_toml:GnModuleSchemaConf",
+            "migrations = gn_module_import:migrations",
+            "tasks = gn_module_import.tasks",
         ],
     },
-    classifiers=['Development Status :: 1 - Planning',
-                 'Intended Audience :: Developers',
-                 'Natural Language :: English',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7',
-                 'Programming Language :: Python :: 3.8',
-                 'License :: OSI Approved :: GNU Affero General Public License v3'
-                 'Operating System :: OS Independent'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU Affero General Public License v3"
+        "Operating System :: OS Independent",
+    ],
 )
